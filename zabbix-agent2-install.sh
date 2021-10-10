7995d942d6545688366efcc3531093bc747cdfea
alv1r <alv1r@ya.ru>
2021-10-10 15:15:28 +0300

    Update zabbix-agent2-install.sh

--- zabbix-agent2-install.sh
+++ zabbix-agent2-install.sh
@@ -1,19 +1,21 @@
-#!/bin/sh
-echo "Installing Zabbix agent2 v5.4"
-# Check Ubuntu version and download proper agent 
-VERSION=$(lsb_release -r -s)
+#!/bin/bash
+if [ -z "$1" ]
+  then
+    echo "No argument supplied. Usage: zabbix-agent2-install.sh IP OR FQDN"
+fi
+echo "Installing Zabbix agent2 v5.0 LTS"
 # Get hostname
 HOSTNAME=$(hostname -s)
-# Get server adress
-SERVER=$1
-wget --quiet http://repo.zabbix.com/zabbix/5.4/ubuntu/pool/main/z/zabbix/zabbix-agent2_5.4.0-1+ubuntu"$VERSION"_amd64.deb
-dpkg -i zabbix-agent2_5.4.0-1+ubuntu"$VERSION"_amd64.deb
-rm zabbix-agent2_5.4.0-1+ubuntu"$VERSION"_amd64.deb
+wget --quiet https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+focal_all.deb
+dpkg -i zabbix-release_5.0-1+focal_all.deb
+apt-get update -y
+rm zabbix-release_5.0-1+focal_all.deb
+apt-get install zabbix-agent2 -y
 echo "PidFile=/var/run/zabbix/zabbix_agentd2.pid
 LogFile=/var/log/zabbix/zabbix_agentd2.log
 LogFileSize=5
-Server="$SERVER"
-ServerActive="$SERVER"
+Server="$1"
+ServerActive="$1"
 Hostname="$HOSTNAME"
 AllowKey=system.run[*]
 Include=/etc/zabbix/zabbix_agent2.d/*.conf" > /etc/zabbix/zabbix_agentd2.conf
